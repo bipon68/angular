cef6ff82489f5aaa54cb0a5826ae2d72bf56f7c4
bipon68 <bipon68@users.noreply.github.com>
2021-01-03 11:27:40 +0600

    two way binding and pipes

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -8,6 +8,16 @@ import { CoursesService } from '../courses.service';
 })
 export class CoursesComponent implements OnInit {
 
+  course = {
+    title: "The Angular Course",
+    rating: 4.9723,
+    students: 30455,
+    price: 160.45,
+    releaseDate: new Date(2016, 3, 1)
+  }
+
+  email = "tomjerry@gmail.com"
+
   isActive = true;
 
   courses;
@@ -23,7 +33,25 @@ export class CoursesComponent implements OnInit {
 
   }
   onSave($event){
+    $event.stopPropagation();
     console.log('Button was clicked!', $event)
+  }
+  onDivClicked(){
+    console.log('Div was clicked!')
+  }
+  // onKeyUp($event){
+  //   if($event.keyCode === 13){
+  //       console.log('Enter was pressed.')
+  //   }
+  // }
+
+  // template variables
+  // onKeyUp($event){
+  //       console.log('Input value data: ', $event.target.value)
+  // }
+
+  onKeyUp(){
+    console.log('Email: ', this.email)
   }
 
 }
