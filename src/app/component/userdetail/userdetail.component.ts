37ea4159ba10da60664ce78e75fc4e0dbad5ed59
bipon68 <bipon68@users.noreply.github.com>
2022-08-22 23:59:33 +0600

    #11 fetch user using uuid

--- src/app/component/userdetail/userdetail.component.ts
+++ src/app/component/userdetail/userdetail.component.ts
@@ -1,4 +1,6 @@
+import { UserService } from './../../service/user.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-userdetail',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserdetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private activatedRoute:ActivatedRoute, private userService: UserService) { }
 
   ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((params: ParamMap) => {
+      console.log('User ID: ', params.get('uuid')!);
+      this.userService.getUser(params.get('uuid')!).subscribe(
+        (response: any) => {
+          console.log(response)
+        }
+      )
+    })
   }
 
 }
